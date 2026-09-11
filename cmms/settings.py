0efe929b4f4be41f4000dab1af389793c2c94cd6
jyoti1305-eng <jyoti@iengaust.com.au>
2026-09-11 14:14:27 +0530

    jyoti-update

--- cmms/settings.py
+++ cmms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tpzqxw&&@03wq2yzgf!gzh6u2=044s2j+_!#jioe(#f^6%quzo'
  
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # keep false on server, true locally if needed
+DEBUG = False  # keep false on server, true locally if needed
  
 if DEBUG:
     SECURE_SSL_REDIRECT = False
@@ -34,7 +34,7 @@ else:
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ["industrialautomation.ieng.tech", ".ieng.tech","*"]
+ALLOWED_HOSTS = ["industrialautomation.ieng.tech", ".ieng.tech"]
  
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
@@ -172,13 +172,13 @@ EMAIL_HOST_USER = 'test@ieng.tech'  # Your email address
 EMAIL_HOST_PASSWORD = 'test@iEng'  # Your email password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CONTACT_RECIPIENTS = [
-  #  "shila@iengaust.com.au",
-   #  "test@ieng.tech",
-   #  "aarti@iengaust.com.au"
-   "jyoti@iengaust.com.au"
+   "shila@iengaust.com.au",
+    "test@ieng.tech",
+    "aarti@iengaust.com.au"
+   #"jyoti@iengaust.com.au"
 ]
-#CONTACT_RECIPIENTS = [ "shila@iengaust.com.au","test@ieng.tech","aarti@iengaust.com.au"]
-CONTACT_RECIPIENTS = [ "jyoti@iengaust.com.au"]
+CONTACT_RECIPIENTS = [ "shila@iengaust.com.au","test@ieng.tech","aarti@iengaust.com.au"]
+#CONTACT_RECIPIENTS = [ "jyoti@iengaust.com.au"]
 DEMO_RECIPIENTS = CONTACT_RECIPIENTS
  
 # CONTACT_EMAIL = 'diksha@iengaust.com.au'
